625c6a4dbd4ee3f2b2aeb36a676fe1d0fac48ce8
SamipTmg614 <samiptamang5614@gmail.com>
2026-04-17 03:35:36 +0545

    more fix

--- website/settings.py
+++ website/settings.py
@@ -44,7 +44,7 @@ CSRF_TRUSTED_ORIGINS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.renserver_nostatic',
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
